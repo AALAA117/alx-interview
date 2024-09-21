85d6d53594aee8bdab7a68ee475e30f76763d649
aalaa <alaamglal@gmail.com>
2024-09-21 16:30:58 +0300

    replace request by axios

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require("request");
+const axios = require("axios");
 
 if (process.argv.length !== 3) {
   console.log("Usage: ./0-starwars_characters.js <film_id>");
@@ -9,33 +9,28 @@ if (process.argv.length !== 3) {
 
 const movieID = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieID}/`;
-request(url, (error, res, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (res.statusCode !== 200) {
-    console.log("Failed To Get Movie");
-    return;
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  exactOrder(characters, 0);
-});
+
+axios
+  .get(url)
+  .then((response) => {
+    const film = response.data;
+    const characters = film.characters;
+    exactOrder(characters, 0);
+  })
+  .catch((error) => {
+    console.error("Failed to get movie:", error.message);
+  });
 
 const exactOrder = (characters, index) => {
   if (index === characters.length) return;
-  request(characters[index], (error, res, body) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    if (res.statusCode !== 200) {
-      console.log("Failed To Get Character");
-      return;
-    }
-    const character = JSON.parse(body);
-    console.log(character.name);
-    exactOrder(characters, index + 1);
-  });
+  axios
+    .get(characters[index])
+    .then((response) => {
+      const character = response.data;
+      console.log(character.name);
+      exactOrder(characters, index + 1);
+    })
+    .catch((error) => {
+      console.error("Failed to get character:", error.message);
+    });
 };
